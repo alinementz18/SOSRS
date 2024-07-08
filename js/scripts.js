ae115ef88e70697504f018607b7c33d54f50dbcf
Aline Mentz <alinementz18@gmail.com>
2024-07-07 22:51:04 -0300

    alterações finais

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        const symptoms = document.getElementById('symptoms').value.toLowerCase();
+        const symptomsInput = document.getElementById('symptoms').value.toLowerCase();
+        const symptomsList = symptomsInput.split(',').map(symptom => symptom.trim()); // Divide os sintomas por vírgula e remove espaços extras
+
         let diagnosis = '';
 
         // Mapeamento de sintomas para doenças
@@ -14,22 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
             'Tétano': ['contrações musculares dolorosas', 'mandíbula', 'pescoço', 'dificuldade para respirar', 'febre', 'pressão alta', 'sudorese'],
             'Diarreia aguda': ['sangramentos nas fezes', 'dores abdominais', 'muco'],
             'Hepatite A': ['fadiga', 'icterícia', 'urina escura', 'fezes claras', 'perda de apetite'],
-            'Dengue': ['febre alta', 'dores musculares', 'erupções cutâneas', 'dores nas articulações', 'hemorragia intensa', 'dificuldade para respirar', 'edema', 'rubor', 'dor de cabeça'],
-            'Animais peçonhentos': ['lugares úmidos', 'escuros', 'entulhos', 'destroços']
+            'Dengue': ['erupções cutâneas', 'dores nas articulações', 'hemorragia intensa', 'dificuldade para respirar', 'edema', 'rubor', 'dor de cabeça'],
+            'Animais peçonhentos': ['dor', 'eritema', 'hematoma', 'bolhas na pele']
         };
 
-        // Função para verificar se todos os sintomas da doença estão presentes
-        const hasAllSymptoms = (disease, symptomsList) => {
-            return diseaseSymptoms[disease].every(symptom => {
-                const regexp = new RegExp('\\b' + symptom + '\\b', 'i'); // Expressão regular para correspondência de palavra inteira
-                return regexp.test(symptomsList);
-            });
+        // Função para verificar se pelo menos um sintoma da doença está presente
+        const hasAnySymptom = (disease, symptomsList) => {
+            return diseaseSymptoms[disease].some(symptom => symptomsList.includes(symptom));
         };
 
         // Verificação dos sintomas fornecidos
         let foundDisease = false;
         for (const disease in diseaseSymptoms) {
-            if (hasAllSymptoms(disease, symptoms)) {
+            if (hasAnySymptom(disease, symptomsList)) {
                 diagnosis = `Possível diagnóstico: ${disease}`;
                 foundDisease = true;
                 break;
